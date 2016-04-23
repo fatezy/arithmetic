b6ae5e7bf39cbbdc58caeb89f879b823738826d2
fatezy <1104199203@qq.com>
2016-04-23 21:03:58 +0800

    202

---
+++ arithmetic/leetCode/hashTableh/IsValidSudoku_36.java
@@ -0,0 +1,18 @@
+package hashTableh;
+
+/**
+ * author： 张亚飞
+ * time：2016/4/23  20:44
+ */
+//Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+//
+//        The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+    //数独小游戏，判断是否数独
+    //// TODO: 2016/4/23
+public class IsValidSudoku_36 {
+    public boolean isValidSudoku(char[][] board) {
+        return false;
+
+    }
+
+}
