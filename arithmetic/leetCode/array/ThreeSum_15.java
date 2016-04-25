416b481cc33f2d300f85042b9fdbcf148065709f
fatezy <zyf_mailz@163.com>
2016-04-25 23:37:17 +0800

    15

---
+++ arithmetic/leetCode/array/ThreeSum_15.java
@@ -0,0 +1,33 @@
+package array;
+
+import javafx.scene.effect.SepiaTone;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * author： 张亚飞
+ * time：2016/4/25  23:19
+ */
+//Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+//
+//        Note:
+//        Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+//        The solution set must not contain duplicate triplets.
+//        For example, given array S = {-1 0 1 2 -1 -4},
+//
+//        A solution set is:
+//        (-1, 0, 1)
+//        (-1, -1, 2)
+
+    //给你一个数组，找出里面三数之和为0的所有组
+public class ThreeSum_15 {
+    //// TODO: 2016/4/25 明天继续 
+        public List<List<Integer>> threeSum(int[] nums) {
+            Arrays.sort(nums);
+
+        return null;
+
+    }
+}
