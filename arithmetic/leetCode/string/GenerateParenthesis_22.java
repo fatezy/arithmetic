d782c58761c67f4974c70c8c3e97bcb0b8dc8947
fatezy <zyf_mailz@163.com>
2016-04-27 15:55:29 +0800

    242

---
+++ arithmetic/leetCode/string/GenerateParenthesis_22.java
@@ -0,0 +1,21 @@
+package string;
+
+import java.util.List;
+
+/**
+ * author： 张亚飞
+ * time：2016/4/27  15:38
+ */
+//Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+//
+//        For example, given n = 3, a solution set is:
+//
+//        "((()))", "(()())", "(())()", "()(())", "()()()"
+    //// TODO: 2016/4/27 配对括号 
+public class GenerateParenthesis_22 {
+    public List<String> generateParenthesis(int n) {
+
+        return null;
+
+    }
+}
