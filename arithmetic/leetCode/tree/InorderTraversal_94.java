2f4a0c534401cb34a31142b2ddbb81a68d1d23e1
fatezy <zyf_mailz@163.com>
2016-05-01 21:40:58 +0800

    94

---
+++ arithmetic/leetCode/tree/InorderTraversal_94.java
@@ -0,0 +1,28 @@
+package tree;
+
+import util.TreeNode;
+
+import java.util.List;
+
+/**
+ * 作者: 张亚飞
+ * 时间: 16-5-1
+ */
+//Given a binary tree, return the inorder traversal of its nodes' values.
+//
+//        For example:
+//        Given binary tree {1,#,2,3},
+//        1
+//        \
+//        2
+//        /
+//        3
+//        return [1,3,2].
+    //中序遍历二叉树
+public class InorderTraversal_94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        return null;
+
+    }
+
+}
--- arithmetic/leetCode/util/TreeNode.java
+++ arithmetic/leetCode/util/TreeNode.java
@@ -7,5 +7,5 @@ public class TreeNode {
         public int val;
         public TreeNode left;
         public  TreeNode right;
-        TreeNode(int x) { val = x; }
+        public TreeNode(int x) { val = x; }
     }
