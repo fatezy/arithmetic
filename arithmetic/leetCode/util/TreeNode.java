f92e79fa1873dea7d9f6665d37491ba24dd28d1a
fatezy <zyf_mailz@163.com>
2016-05-01 22:36:09 +0800

    Tree

---
+++ arithmetic/leetCode/util/TreeNode.java
@@ -0,0 +1,12 @@
+package util;
+
+/**
+ * author： 张亚飞
+ * time：2016/5/1  17:02
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
