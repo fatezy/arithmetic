fb94daff1099fb423aaf0c195d12af70169fff0c
fatezy <zyf_mailz@163.com>
2016-05-01 16:40:19 +0800

    287

--- arithmetic/leetCode/BinarySearch/FindDuplicate_287.java
+++ arithmetic/leetCode/BinarySearch/FindDuplicate_287.java
@@ -31,4 +31,29 @@ public class FindDuplicate_287 {
         return 0;
     }
 
+    //变异的二分查找法,貌似并没有提高时间复杂度，依旧哦（oN2）
+    public int findDuplicate2(int[] nums) {
+        if ( nums ==null ||nums.length==0){
+            return 0;
+        }
+        int low = 1, high = nums.length-1, mid;
+        int count=0;
+        while (low<=high){
+            mid = low + (high - low) / 2;
+            count  = 0;
+            for (int i =0;i<nums.length;i++){
+                if (nums[i] <= mid){
+                    count++;
+                }
+            }
+            if (count>mid){
+                high = mid;
+            }else {
+               low = mid +1 ;
+            }
+        }
+        return low;
+    }
+
+
 }
