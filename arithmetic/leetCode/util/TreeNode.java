5fdd77f1132bcfca8ba455fe6b8e0c06cf4ec3c5
fatezy <zyf_mailz@163.com>
2016-05-01 20:19:14 +0800

    Merge branch 'master' of https://github.com/fatezy/arithmetic
    Conflicts:
    	arithmetic/arithmetic.iml

---
+++ arithmetic/leetCode/util/TreeNode.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * Created by zhangyafei on 16-5-1.
+ */
+public class TreeNode {
+        int val;
+       public TreeNode left;
+        public  TreeNode right;
+    TreeNode(int x) { val = x; }
+    }
