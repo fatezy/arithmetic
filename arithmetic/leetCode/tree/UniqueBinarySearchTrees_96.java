fd5a6e2d01ff18ec6e868a483ca9875b0d2f04e8
fatezy <zyf_mailz@163.com>
2016-05-02 09:46:42 +0800

    96

---
+++ arithmetic/leetCode/tree/UniqueBinarySearchTrees_96.java
@@ -0,0 +1,25 @@
+package tree;
+
+/**
+ * author： 张亚飞
+ * time：2016/5/2  09:47
+ */
+//Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+//
+//        For example,
+//        Given n = 3, there are a total of 5 unique BST's.
+//
+//        1         3     3      2      1
+//        \       /     /      / \      \
+//        3     2     1      1   3      2
+//        /     /       \                 \
+//        2     1         2                 3
+    //判断二叉排序树又多少种重构
+    //貌似是动态规划的问题，先放弃
+    //// TODO: 2016/5/2
+public class UniqueBinarySearchTrees_96 {
+    public int numTrees(int n) {
+        return 0;
+    }
+
+}
