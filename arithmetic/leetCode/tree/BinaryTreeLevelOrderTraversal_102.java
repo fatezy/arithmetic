e1eb2651de078db368fec430984b6b971c0d4da2
fatezy <zyf_mailz@163.com>
2016-05-02 12:13:56 +0800

    102

---
+++ arithmetic/leetCode/tree/BinaryTreeLevelOrderTraversal_102.java
@@ -0,0 +1,37 @@
+package tree;
+
+import util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * author： 张亚飞
+ * time：2016/5/2  10:14
+ */
+//Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+//
+//        For example:
+//        Given binary tree {3,9,20,#,#,15,7},
+//        3
+//        / \
+//        9  20
+//        /  \
+//        15   7
+//        return its level order traversal as:
+//        [
+//        [3],
+//        [9,20],
+//        [15,7]
+//        ]
+
+    //二叉搜索树，层序遍历
+    //// TODO: 2016/5/2
+public class BinaryTreeLevelOrderTraversal_102 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+
+        return null;
+    }
+}
