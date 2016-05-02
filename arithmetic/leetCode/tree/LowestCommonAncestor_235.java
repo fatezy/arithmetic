df7984989c9c540ef45c9503b4c035257d0835c0
fatezy <zyf_mailz@163.com>
2016-05-02 09:46:42 +0800

    235

--- arithmetic/leetCode/tree/LowestCommonAncestor_235.java
+++ arithmetic/leetCode/tree/LowestCommonAncestor_235.java
@@ -19,11 +19,14 @@ import util.TreeNode;
 //        3   5
 //        For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
     //找二叉搜索树的共同祖先
-    //// TODO: 2016/5/1  
+     //先序遍历
+    //最小的祖先一定大于其中一个小于另外一个
 public class LowestCommonAncestor_235 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-
-        return null;
+        if(p.val>q.val)
+            return lowestCommonAncestor(root, q,p);
+        if(root.val>= p.val && root.val<=q.val)
+            return root;
+        return lowestCommonAncestor(root.val>p.val?root.left:root.right, p,q);
     }
 }
