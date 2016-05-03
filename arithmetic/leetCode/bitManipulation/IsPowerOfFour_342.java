43b5fe239e119a2e30e90c4979b9f62e5801cc98
fatezy <zyf_mailz@163.com>
2016-05-03 22:24:17 +0800

    137

--- arithmetic/leetCode/bitManipulation/IsPowerOfFour_342.java
+++ arithmetic/leetCode/bitManipulation/IsPowerOfFour_342.java
@@ -14,9 +14,21 @@ public class IsPowerOfFour_342 {
         if ((num & 0x55555555) != 0) return true;
         return false;
     }
+
     //num == (num & -num)判断末位是否为0，为0的是奇数
+    //求负就是 例如:5的二进制是101，五的反码就是10，那么10的补码是11故 -5就是11
+    //num & -num 如果是奇数一定是为1 ，如果是2的次方一定是他本身，其余不一定
     public boolean isPowerOfFour3(int num) {
         return num > 0 && num == (num & -num) && num == (num & 0x55555555);
     }
 
+
+    public static void main(String[] args) {
+        int a = 135;
+        System.out.println(a==(a& -a));
+        System.out.println((a& -a));
+    }
+
 }
+
+
