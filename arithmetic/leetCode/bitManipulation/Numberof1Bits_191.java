be5a1beeb2cc236cbd18cdec52b1b1dbdf82c7d3
fatezy <zyf_mailz@163.com>
2016-05-03 23:00:12 +0800

    191

--- arithmetic/leetCode/bitManipulation/Numberof1Bits_191.java
+++ arithmetic/leetCode/bitManipulation/Numberof1Bits_191.java
@@ -1,5 +1,7 @@
 package bitManipulation;
 
+import org.junit.Test;
+
 /**
  * author： 张亚飞
  * time：2016/5/2  17:21
@@ -7,9 +9,20 @@ package bitManipulation;
 //Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
 //
 //        For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+//判断二进制数中1的个数
 public class Numberof1Bits_191 {
-    public int hammingWeight(int n) {
-        return 0;
+    public static int hammingWeight(int n) {
+        int res = 0;
+        for (int i = -1; i <31 ; i++) {
+            if (((n >> i)&1) == 1 ){
+                res++;
+            }
 
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hammingWeight(11));
     }
 }
