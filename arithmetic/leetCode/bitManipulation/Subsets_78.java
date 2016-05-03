c131859a6affae5645be7caa45ea536e2b7c5580
fatezy <zyf_mailz@163.com>
2016-05-03 23:45:12 +0800

    78明天继续

---
+++ arithmetic/leetCode/bitManipulation/Subsets_78.java
@@ -0,0 +1,20 @@
+package bitManipulation;
+
+import java.util.List;
+
+/**
+ * author： 张亚飞
+ * time：2016/5/3  23:39
+ */
+//Given a set of distinct integers, nums, return all possible subsets.
+//
+//        Note:
+//        Elements in a subset must be in non-descending order.
+//        The solution set must not contain duplicate subsets.
+    //求子集，返回结果单个子集合必须递增
+public class Subsets_78 {
+    public List<List<Integer>> subsets(int[] nums) {
+        return null;
+
+    }
+}
