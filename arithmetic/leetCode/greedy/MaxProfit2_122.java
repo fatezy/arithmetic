44b28d2a84a21317ad351d5fa2e58407dee33d1a
fatezy <zyf_mailz@163.com>
2016-05-04 23:36:07 +0800

    263

--- arithmetic/leetCode/greedy/MaxProfit2_122.java
+++ arithmetic/leetCode/greedy/MaxProfit2_122.java
@@ -10,8 +10,18 @@ package greedy;
 // (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple
 // transactions at the same time
 //(ie, you must sell the stock before you buy again).
+//用一个数组表示股票每天的价格，数组的第i个数表示股票在第i天的价格。
+    //一次只能操作一只股票，可多次交易，求最大的收益
 public class MaxProfit2_122 {
     public int maxProfit(int[] prices) {
-        return 0;
+        int profit = 0;
+        if (prices.length<2)
+            return 0;
+        for (int i = 1; i <prices.length ; i++) {
+            profit += ( prices[i]>prices[i-1] ? prices[i]-prices[i-1] : 0);
+        }
+        return profit;
+
+
     }
 }
