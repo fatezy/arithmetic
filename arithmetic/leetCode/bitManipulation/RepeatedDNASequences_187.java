cdc6149385f4fdea194921577ab7dabca2eaa6dc
fatezy <zyf_mailz@163.com>
2016-05-05 19:17:21 +0800

    187

--- arithmetic/leetCode/bitManipulation/RepeatedDNASequences_187.java
+++ arithmetic/leetCode/bitManipulation/RepeatedDNASequences_187.java
@@ -31,4 +31,36 @@ public class RepeatedDNASequences_187 {
         res.addAll(resset);
         return res;
     }
+
+
+    public List<String> findRepeatedDnaSequences2(String s) {
+        Set<Integer> firstTime = new HashSet<Integer>();
+        Set<Integer> secondTime = new HashSet<Integer>();
+        List<String> list = new ArrayList<String>();
+
+        char[] map = new char[26];
+        int len = s.length();
+
+        map['A' - 'A'] = 0; // A = 00
+        map['C' - 'A'] = 1; // B = 01
+        map['G' - 'A'] = 2; // C = 10
+        map['T' - 'A'] = 3; // D = 11
+
+        for(int i=0; i<= len - 10; i++) {
+            int sequence = 0;
+            //构建hash值
+            for(int j=i; j< i+10; j++) {
+                sequence = sequence << 2;
+                sequence = sequence | map[s.charAt(j) - 'A'];
+            }
+            //firstTime添加成功，则短路第二次
+            //第二次添加时，firsttime添加失败
+            if(!firstTime.add(sequence) && secondTime.add(sequence)) {
+                list.add(s.substring(i, i+10));
+            }
+        }
+        return list;
+    }
+
+
 }
