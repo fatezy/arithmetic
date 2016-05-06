beb2a0039c6a45e296c03dc06c6cbfd74bf583b6
fatezy <zyf_mailz@163.com>
2016-05-06 17:40:36 +0800

    42

---
+++ arithmetic/leetCode/stack/ImplementQueueusingStacks_232.java
@@ -0,0 +1,36 @@
+package stack;
+
+/**
+ * author： 张亚飞
+ * time：2016/5/6  15:55
+ */
+//Implement the following operations of a queue using stacks.
+//
+//        push(x) -- Push element x to the back of queue.
+//        pop() -- Removes the element from in front of queue.
+//        peek() -- Get the front element.
+//        empty() -- Return whether the queue is empty.
+    //用栈实现一个队列
+public class ImplementQueueusingStacks_232 {
+    // Push element x to the back of queue.
+    public void push(int x) {
+
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+
+    }
+
+    // Get the front element.
+    public int peek() {
+        return 1;
+
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return false;
+
+    }
+}
---
+++ arithmetic/leetCode/stack/TrappingRainWater_42.java
@@ -0,0 +1,17 @@
+package stack;
+
+/**
+ * author： 张亚飞
+ * time：2016/5/6  16:01
+ */
+//Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+//
+//        For example,
+//        Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+    //求陷阱中可以放多少水
+public class TrappingRainWater_42 {
+    public int trap(int[] height) {
+        return 0;
+
+    }
+}
