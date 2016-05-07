7b554982701c727f84dbaf0c2ffded9864fdefbe
fatezy <zyf_mailz@163.com>
2016-05-07 15:21:52 +0800

    75

--- arithmetic/leetCode/stack/ImplementQueueusingStacks_232.java
+++ arithmetic/leetCode/stack/ImplementQueueusingStacks_232.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.Stack;
+
 /**
  * author： 张亚飞
  * time：2016/5/6  15:55
@@ -12,25 +14,43 @@ package stack;
 //        empty() -- Return whether the queue is empty.
     //用栈实现一个队列
 public class ImplementQueueusingStacks_232 {
+    Stack<Integer> s1=new Stack<Integer>();
+    Stack<Integer> s2=new Stack<Integer>();
     // Push element x to the back of queue.
     public void push(int x) {
+        s1.push(x);
 
     }
 
     // Removes the element from in front of queue.
     public void pop() {
+        if (!s2.empty()){
+            s2.pop();
+        }else {
+            while (!s1.empty()){
+                s2.push(s1.pop());
+            }
+            s2.pop();
+        }
 
     }
 
-    // Get the front element.
     public int peek() {
-        return 1;
+        if(!s2.empty())
+            return s2.peek();
+        else{
+            while(!s1.empty())
+                s2.push(s1.pop());
 
+            return s2.peek();
+        }
     }
 
     // Return whether the queue is empty.
     public boolean empty() {
-        return false;
-
+        if(s1.empty()&&s2.empty())
+            return true;
+        else
+            return false;
     }
 }
