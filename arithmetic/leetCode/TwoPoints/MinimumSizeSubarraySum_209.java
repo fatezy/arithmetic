0f6b48a82be7a57f65f3586721352dfea7e7bb10
fatezy <zyf_mailz@163.com>
2016-05-14 21:21:38 +0800

    209

---
+++ arithmetic/leetCode/TwoPoints/MinimumSizeSubarraySum_209.java
@@ -0,0 +1,30 @@
+package TwoPoints;
+
+/**
+ * author： 张亚飞
+ * time：2016/5/14  21:00
+ */
+//Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+//
+//        For example, given the array [2,3,1,2,4,3] and s = 7,
+//        the subarray [4,3] has the minimal length under the problem constraint.
+    //找出最短的子数组和大于目标值
+    //滑动窗口法，最左边弄两个指针,一快一慢先后移动
+public class MinimumSizeSubarraySum_209 {
+    public int minSubArrayLen(int s, int[] nums) {
+        if (nums == null || nums.length <1){
+            return 0;
+        }
+        int start =0 , end = 0 , sum =0, min = Integer.MAX_VALUE;
+        while (end <nums.length){
+            sum += nums[end];
+            while (sum>=s){
+                min = Math.min(min,end-start+1);
+                sum -= nums[start++];
+            }
+            end++;
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+
+}
---
+++ arithmetic/leetCode/linkList/SortedListToBST_109.java
@@ -0,0 +1,36 @@
+package linkList;
+
+import util.ListNode;
+import util.TreeNode;
+
+/**
+ * author： 张亚飞
+ * time：2016/5/14  20:42
+ */
+//Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+    //给你一个有序链表把它转化为一个平衡二叉树
+    //// TODO: 2016/5/14
+    //回头再看
+public class SortedListToBST_109 {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head==null){
+            return null;
+        }
+        if(head.next==null){
+            TreeNode treeNode=new TreeNode(head.val);
+            return treeNode;
+        }
+        ListNode slow=head;
+        ListNode fast=head.next.next;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        TreeNode root=new TreeNode(slow.next.val);
+        ListNode temp=slow.next.next;
+        slow.next=null;
+        root.left=sortedListToBST(head);
+        root.right=sortedListToBST(temp);
+        return root;
+    }
+}
