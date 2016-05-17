10411da92721ae86d7b7c430ab8f779417cc7207
fatezy <zyf_mailz@163.com>
2016-05-17 21:46:56 +0800

    10

--- arithmetic/offer/JumpFloorII_09.java
+++ arithmetic/offer/JumpFloorII_09.java
@@ -6,7 +6,7 @@
 
 //一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
 // 求该青蛙跳上一个n级的台阶总共有多少种跳法。
-public class JumpFloorII_11 {
+public class JumpFloorII_09 {
     public int JumpFloorII(int target) {
         if (target <= 0) {
             return -1;
--- arithmetic/offer/RectCover_09.java
+++ arithmetic/offer/RectCover_09.java
@@ -5,7 +5,7 @@
 //我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
 // 请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
     //本题其实还是fibonacci·的变种
-public class RectCover_12 {
+public class RectCover_09 {
     public int RectCover(int target) {
         if (target ==0){
             return 0;
