5b024dbb6e42aaeb84ce665b9fd411e07eda22d8
fatezy <zyf_mailz@163.com>
2016-05-18 22:45:44 +0800

    14

---
+++ arithmetic/offer/ReOrderArray_14.java
@@ -0,0 +1,18 @@
+/**
+ * author： 张亚飞
+ * time：2016/5/17  22:04
+ */
+//题目描述
+
+//        输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分
+// ，所有的偶数位于位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+public class ReOrderArray_14 {
+    public void reOrderArray(int [] array) {
+        if (array.length == 0){
+            return;
+        }
+
+
+    }
+
+}
