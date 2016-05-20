8f1feb01252c911fe184de6b114c55000e588e74
fatezy <zyf_mailz@163.com>
2016-05-20 20:19:46 +0800

    逆转链表

---
+++ arithmetic/offer/ReverseList.java
@@ -0,0 +1,36 @@
+import util.Link_List;
+import util.ListNode;
+
+/**
+ * author： 张亚飞
+ * time：2016/5/20  17:38
+ */
+//逆转链表
+public class ReverseList {
+    public static ListNode ReverseList(ListNode head) {
+        if ((head == null) || (head.next == null)){
+            return head;
+        }
+
+         ListNode curr = head;
+        ListNode pre = null;
+        ListNode nextNode ;
+        while(curr!=null){
+            nextNode = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = nextNode;
+        }
+        return pre;
+
+    }
+
+    public static void main(String[] args) {
+        Link_List list = new Link_List();
+        list.initList(1,2,3,4,5);
+        ReverseList(list.getFirstNode());
+        System.out.println("ok");
+    }
+
+
+}
