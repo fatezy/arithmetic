ee5436834ab99dd2c9b933f37e049886fbf193b8
fatezy <zyf_mailz@163.com>
2016-05-20 21:52:45 +0800

    23

---
+++ arithmetic/offer/PrintFromTopToBottom_23.java
@@ -0,0 +1,28 @@
+import util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * author： 张亚飞
+ * time：2016/5/20  21:36
+ */
+//层序遍历
+public class PrintFromTopToBottom_23 {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null){
+            return list;
+        }
+        queue.offer(root);
+        while (queue.size()!=0){
+            TreeNode temp = queue.poll();
+            list.add(temp.val);
+            if (temp.left != null) queue.add(temp.left);
+            if (temp.right!=null) queue.add(temp.right);
+        }
+        return list;
+    }
+}
