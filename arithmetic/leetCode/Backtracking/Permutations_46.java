898e7eaeecf323b3cd8c7f254c2ebf8628301743
fatezy <zyf_mailz@163.com>
2016-06-02 09:50:23 +0800

    46

--- arithmetic/leetCode/Backtracking/Permutations_46.java
+++ arithmetic/leetCode/Backtracking/Permutations_46.java
@@ -1,6 +1,7 @@
 package Backtracking;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -49,5 +50,30 @@ public class Permutations_46 {
         }
 
         return total;
+    }
+
+
+
+    public static List<List<Integer>> permute2(int[] num) {
+        LinkedList<List<Integer>> res = new LinkedList<List<Integer>>();
+        res.add(new ArrayList<Integer>());
+        for (int n : num) {
+            int size = res.size();
+            for (; size > 0; size--) {
+                List<Integer> r = res.pollFirst();
+                for (int i = 0; i <= r.size(); i++) {
+                    List<Integer> t = new ArrayList<Integer>(r);
+                    t.add(i, n);
+                    res.add(t);
+                }
+            }
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3};
+        permute2(a);
     }
 }
