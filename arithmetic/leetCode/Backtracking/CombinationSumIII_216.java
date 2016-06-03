8020c27b4abd7806fe4ed63f30d59258b41e1d74
fatezy <1104199203@qq.com>
2016-06-03 20:07:18 +0800

    40

---
+++ arithmetic/leetCode/Backtracking/CombinationSumIII_216.java
@@ -0,0 +1,13 @@
+package Backtracking;
+
+/**
+ * author： 张亚飞
+ * time：2016/6/3  20:06
+ */
+//Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+//
+//        Ensure that numbers within the set are sorted in ascending order.
+//
+
+public class CombinationSumIII_216 {
+}
