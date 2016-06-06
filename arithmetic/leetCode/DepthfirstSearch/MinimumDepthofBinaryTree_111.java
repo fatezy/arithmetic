39ca6c0beb67af04f4d60f25075d62cae8e84f11
fatezy <zyf_mailz@163.com>
2016-06-06 09:12:14 +0800

    111

--- arithmetic/leetCode/DepthfirstSearch/MinimumDepthofBinaryTree_111.java
+++ arithmetic/leetCode/DepthfirstSearch/MinimumDepthofBinaryTree_111.java
@@ -13,22 +13,28 @@ import util.TreeNode;
     //求二叉树的最小深度
     //// TODO: 2016/6/4  
 public class MinimumDepthofBinaryTree_111 {
-    static int depth = 0;
     static int minDepth = Integer.MAX_VALUE;
-    public int minDepth(TreeNode root) {
-        return 0;
+    public static int minDepth(TreeNode root) {
+
+        return getDepth(root);
     }
 
-    public int getDepth(TreeNode root){
-        if (root == null) {
-            if (depth < minDepth) {
-                minDepth = depth;
-                return depth;
-            }
+    public static int getDepth(TreeNode root){
+        if (root == null){
+            return 0;
         }
-        depth++;
-        
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+        if ((left==0||right==0)&&(left!=0||right!=0))
+            return Math.max(left,right)+1;
+        return Math.min(left,right)+1;
 
-        return depth;
+    }
+
+    public static void main(String[] args) {
+        TreeNode tree  = new TreeNode(1);
+        tree.left = new TreeNode(2);
+       int a = minDepth(tree);
+        System.out.println(a);
     }
 }
