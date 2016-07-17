d3ad2cef7439681136b18753b5bae4b077858c01
fatezy <zyf_mailz@163.com>
2016-07-17 19:30:44 +0800

    371

---
+++ arithmetic/leetCode/bitManipulation/SumofTwoIntegers_371.java
@@ -0,0 +1,21 @@
+package bitManipulation;
+
+/**
+ * author： 张亚飞
+ * time：2016/7/17  16:39
+ */
+//Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+//
+//        Example:
+//        Given a = 1 and b = 2, return 3.
+    //不可以使用+进行加法
+public class SumofTwoIntegers_371 {
+    public static int getSum(int a, int b) {
+
+        return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSum(20, 30));
+    }
+}
