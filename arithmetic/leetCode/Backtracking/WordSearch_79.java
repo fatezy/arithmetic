1e34f6576fd0f274a8c06ef72a9ebf356e335a28
fatezy <zyf_mailz@163.com>
2016-07-23 17:23:53 +0800

    82 移除重复的数

---
+++ arithmetic/leetCode/Backtracking/WordSearch_79.java
@@ -0,0 +1,29 @@
+package Backtracking;
+
+/**
+ * author： 张亚飞
+ * time：2016/7/21  21:23
+ */
+//Given a 2D board and a word, find if the word exists in the grid.
+//
+//        The word can be constructed from letters of sequentially adjacent cell,
+// where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell
+// may not be used more than once.
+//
+//        For example,
+//        Given board =
+//
+//        [
+//        ['A','B','C','E'],
+//        ['S','F','C','S'],
+//        ['A','D','E','E']
+//        ]
+//        word = "ABCCED", -> returns true,
+//        word = "SEE", -> returns true,
+//        word = "ABCB", -> returns false.
+// TODO: 2016/7/21  
+public class WordSearch_79 {
+    public boolean exist(char[][] board, String word) {
+        return false;
+    }
+}
