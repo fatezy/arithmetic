eefe1cdb687f82d71072afffa03ffa2ca1fb6e17
fatezy <zyf_mailz@163.com>
2016-07-27 10:27:47 +0800

    3

--- arithmetic/leetCode/string/LongestSubstringWithoutRepeatingCharacters_3.java
+++ arithmetic/leetCode/string/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -1,5 +1,7 @@
 package string;
 
+import java.util.HashMap;
+
 /**
  * author： 张亚飞
  * time：2016/6/15  23:29
@@ -15,9 +17,26 @@ package string;
 //        Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer
 // must be a substring, "pwke" is a subsequence and not a substring.
     //找出给定字符串的最长的无重复字符的子串
-// TODO: 2016/6/25
 public class LongestSubstringWithoutRepeatingCharacters_3 {
-    public int lengthOfLongestSubstring(String s) {
-        return 1;
+
+
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max=0;
+        for (int i=0, j=0; i<s.length(); i++){
+            if (map.containsKey(s.charAt(i))){
+                j = Math.max(j,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i-j+1);
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        lengthOfLongestSubstring("cbabert");
     }
 }
