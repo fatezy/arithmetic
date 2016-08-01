2550bfe421fe89b47047219bd4a30c2a8446c3cf
fatezy <zyf_mailz@163.com>
2016-08-01 20:37:25 +0800

    349

---
+++ arithmetic/Crackingthecodinginterview/Reverse_2.java
@@ -0,0 +1,13 @@
+/**
+ * author： 张亚飞
+ * time：2016/8/1  20:32
+ */
+public class Reverse_2 {
+    public String reverseString(String iniString) {
+        if (iniString == null || iniString.length() == 0 ){
+            return iniString;
+        }
+       StringBuffer stringBuffer = new StringBuffer(iniString);
+        return stringBuffer.reverse().toString();
+    }
+}
