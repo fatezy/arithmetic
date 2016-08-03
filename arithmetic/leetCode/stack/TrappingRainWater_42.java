f9b5a78782c3f23ff48e0a8932e22ab31674a98a
fatezy <zyf_mailz@163.com>
2016-08-03 17:06:50 +0800

    42

--- arithmetic/leetCode/stack/TrappingRainWater_42.java
+++ arithmetic/leetCode/stack/TrappingRainWater_42.java
@@ -14,4 +14,39 @@ public class TrappingRainWater_42 {
         return 0;
 
     }
+
+
+    public static int trap2(int[] A) {
+        if (A.length < 3) return 0;
+
+        int ans = 0;
+        int l = 0, r = A.length - 1;
+
+        // find the left and right edge which can hold water
+        while (l < r && A[l] <= A[l + 1]) l++;
+        while (l < r && A[r] <= A[r - 1]) r--;
+
+        while (l < r) {
+            int left = A[l];
+            int right = A[r];
+            if (left <= right) {
+                // add volum until an edge larger than the left edge
+                while (l < r && left >= A[++l]) {
+                    ans += left - A[l];
+                }
+            } else {
+                // add volum until an edge larger than the right volum
+                while (l < r && A[--r] <= right) {
+                    ans += right - A[r];
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,2,1,0,1,3,2,1,2,1};
+        trap2(nums);
+    }
 }
