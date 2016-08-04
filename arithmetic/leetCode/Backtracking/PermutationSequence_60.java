33c8f6c23bbf2b84a5944d0a5c996b11a845a687
fatezy <zyf_mailz@163.com>
2016-08-04 11:56:08 +0800

    60

--- arithmetic/leetCode/Backtracking/PermutationSequence_60.java
+++ arithmetic/leetCode/Backtracking/PermutationSequence_60.java
@@ -1,7 +1,7 @@
 package Backtracking;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+
 import java.util.List;
 
 /**
@@ -26,12 +26,40 @@ import java.util.List;
 
     //给你个数n，得到它的全排列，然后得到第k个排列
 
-// TODO: 2016/7/20  
 public class PermutationSequence_60 {
 
 
     public static String getPermutation(int n, int k) {
-      return null;
+        int pos = 0;
+        List<Integer> numbers = new ArrayList<>();
+        int[] factorial = new int[n+1];
+        StringBuilder sb = new StringBuilder();
+
+        // create an array of factorial lookup
+        int sum = 1;
+        factorial[0] = 1;
+        for(int i=1; i<=n; i++){
+            sum *= i;
+            factorial[i] = sum;
+        }
+        // factorial[] = {1, 1, 2, 6, 24, ... n!}
+
+        // create a list of numbers to get indices
+        for(int i=1; i<=n; i++){
+            numbers.add(i);
+        }
+        // numbers = {1, 2, 3, 4}
+
+        k--;
+
+        for(int i = 1; i <= n; i++){
+            int index = k/factorial[n-i];
+            sb.append(String.valueOf(numbers.get(index)));
+            numbers.remove(index);
+            k-=index*factorial[n-i];
+        }
+
+        return String.valueOf(sb);
     }
 
 
