23c70c0ec86586e540a7caaad1e4d0eaa6fc2f70
fatezy <1104199203@qq.com>
2016-09-20 10:47:22 +0800

    84

--- arithmetic/LeedCodeSecond/Array/FirstMissingPositive_41.java
+++ arithmetic/LeedCodeSecond/Array/FirstMissingPositive_41.java
@@ -29,7 +29,29 @@ public class FirstMissingPositive_41 {
         return nums.length;
     }
 
+
+
+    public static int firstMissingPositive2(int[] A) {
+        int i = 0;
+        while(i < A.length){
+            if(A[i] == i+1 || A[i] <= 0 || A[i] > A.length) i++;
+            else if(A[A[i]-1] != A[i]) swap(A, i, A[i]-1);
+            else i++;
+        }
+        i = 0;
+        while(i < A.length && A[i] == i+1) i++;
+        return i+1;
+    }
+
+    private  static void swap(int[] A, int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
     public static void main(String[] args) {
+        int[] nums = {3,4,-1,1};
+        firstMissingPositive2(nums);
 
     }
 }
