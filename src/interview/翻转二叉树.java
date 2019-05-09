cf1d1e6c67d39d32b5e68ec83d2f9cab946f8104
fatezy <1104199203@qq.com>
2019-05-09 16:31:55 +0800

    反转二叉树

---
+++ src/interview/翻转二叉树.java
@@ -0,0 +1,24 @@
+package interview;
+
+import leetcode.util.TreeNode;
+
+/**
+ * Created by 张亚飞 on 2019/5/9.
+ */
+public class 翻转二叉树 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root != null){
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+
+            invertTree(root.left);
+            invertTree(root.right);
+        }
+
+        return root;
+
+    }
+}
+
+ 
