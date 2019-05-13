6d4583b48f4a06573084f1e34621284bb88f20d7
fatezy <1104199203@qq.com>
2019-05-13 10:33:59 +0800

    面试指南递归写法

--- src/interview/反转链表.java
+++ src/interview/反转链表.java
@@ -8,6 +8,8 @@ import leetcode.util.ListNode;
  * Created by 张亚飞 on 2019/5/9.
  */
 public class 反转链表 {
+
+    //常规方法
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
@@ -20,6 +22,30 @@ public class 反转链表 {
         }
         return prev;
     }
+
+    // 递归
+
+    /**
+     * 递归第一步要考虑当前的函数有什么功能，这个问题下递归即反转当前的链表
+     * 返回值是节点的新头
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head){
+        if (head == null || head.next == null){
+            return head;
+        }
+
+        //返回的是节点的新脑袋
+         ListNode newList =  reverseList2(head.next);
+         ListNode t1 = head.next;
+         t1.next = head;
+         head.next = null;
+
+
+        return newList;
+    }
+
 
     public static void main(String[] args) {
         Link_List list = new Link_List();
