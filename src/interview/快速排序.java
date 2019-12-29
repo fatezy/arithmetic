4200347976fee4c352af36f72546e4597d716d88
张亚飞 <zyf_mailz@163.com>
2019-12-29 16:44:44 +0800

    update

--- src/interview/快速排序.java
+++ src/interview/快速排序.java
@@ -7,74 +7,48 @@ import java.util.Arrays;
  */
 public class 快速排序 {
 
-    public int[] quickSort(int[] arr){
+    public int[] quickSort(int[] arr) {
         int length = arr.length;
-//        partition(arr,0,length-1);
-        partition2(arr,0,length-1);
+        partition(arr,0,length-1);
 
         return arr;
     }
 
     /**
      * 将arr[l]放入正确的位置
+     *
      * @param arr
      * @param l
      * @param r
      * @return
      */
-    public int[] partition(int[] arr,int l,int r){
-        if (l<r){
-            int i =l,j=r,x=arr[l];
-
-            while (i<j){
-                while (arr[j]>=x&&i<j){
-                    j--;
-                }
-                if (i<j){
-                    arr[i++] = arr[j];
-                }
-                while (i<j&&arr[i]<x){
-                    i++;
-                }
-                if (i<j){
-                    arr[j--] = arr[i];
-                }
-            }
-
-            arr[i] = x;
-            partition(arr,l,i-1);
-            partition(arr,i+1,r);
-
-
+    public int[] partition(int[] arr, int l, int r) {
+        if (l >= r) {
+            return arr;
         }
+        int i = l, j = r, x = arr[l];
+
+        while (i < j) {
+            while (arr[j] >= x && i < j) {
+                j--;
+            }
+            if (i < j) {
+                arr[i++] = arr[j];
+            }
+            while (i < j && arr[i] < x) {
+                i++;
+            }
+            if (i < j) {
+                arr[j--] = arr[i];
+            }
+        }
+
+        arr[i] = x;
+        partition(arr, l, i - 1);
+        partition(arr, i + 1, r);
+
         return arr;
     }
-
-
-    public int[] partition2(int[] arr,int l,int r){
-        if(l<r){
-            int i=l,j=r,x = arr[l];
-            while (i<j){
-                while (i<j&&arr[j]>=x){
-                    j--;
-                }
-                if (i<j){
-                    arr[i++] = arr[j];
-                }
-                while (i<j&&arr[i]<x){
-                    i++;
-                }
-                if (i<j){
-                    arr[j--] = arr[i];
-                }
-            }
-            arr[i] = x;
-            partition2(arr,l,i-1);
-            partition2(arr,i+1,r);
-        }
-        return arr;
-    }
-
 
 
 
@@ -82,7 +56,7 @@ public class 快速排序 {
 
     public static void main(String[] args) {
         快速排序 test = new 快速排序();
-        int[] arr ={2,3,1,7,6};
+        int[] arr = {2, 3, 1, 7, 6};
         System.out.println(Arrays.toString(test.quickSort(arr)));
     }
 }
